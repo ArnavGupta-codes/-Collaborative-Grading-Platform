6cc60818f4e20a300406c3f5bf70306a1351de2f
ArnavGupta-codes <guptaarnav631@gmail.com>
2025-01-19 17:13:13 +0530

    New Changes

--- student-center.js
+++ student-center.js
@@ -7,19 +7,17 @@ document.addEventListener('DOMContentLoaded', function () {
             const cardText = e.target.getAttribute('data-card');
             hoverCard.textContent = cardText;
 
-            // Get the position of the hovered link
+            
             const rect = e.target.getBoundingClientRect();
 
-            // Position the card relative to the hovered link
             hoverCard.style.left = `${rect.left}px`;
             hoverCard.style.top = `${rect.bottom + 5}px`;
 
-            // Show the card
             hoverCard.classList.add('show');
         });
 
         link.addEventListener('mouseleave', () => {
-            // Hide the card
+          
             hoverCard.classList.remove('show');
         });
     });
